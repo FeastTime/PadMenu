60753a1ecb7076bf761f23445c1c14cc5527fade
Chen <38033919@qq.com>
2017-08-26 17:39:33 +0800

    修改ui

--- menu/src/main/java/com/feasttime/adapter/HorizontalListViewAdapter.java
+++ menu/src/main/java/com/feasttime/adapter/HorizontalListViewAdapter.java
@@ -6,9 +6,7 @@ package com.feasttime.adapter;
 
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.media.ThumbnailUtils;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,11 +49,14 @@ public class HorizontalListViewAdapter extends BaseAdapter{
             convertView = mInflater.inflate(R.layout.main_menu_recommend_listview_item, null);
 //            holder.mImage=(ImageView)convertView.findViewById(R.id.img_list_item);
 //            holder.mTitle=(TextView)convertView.findViewById(R.id.text_list_item);
+            holder.priceTv = (TextView)convertView.findViewById(R.id.main_menu_recommend_listview_item_price_tv);
+            holder.originlPriceTv = (TextView)convertView.findViewById(R.id.main_menu_recommend_listview_item_original_price_tv);
             convertView.setTag(holder);
         }else{
             holder=(ViewHolder)convertView.getTag();
         }
 
+        holder.originlPriceTv.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
 
         return convertView;
     }
@@ -63,5 +64,7 @@ public class HorizontalListViewAdapter extends BaseAdapter{
     private static class ViewHolder {
         private TextView mTitle ;
         private ImageView mImage;
+        private TextView priceTv;
+        private TextView originlPriceTv;
     }
 }
