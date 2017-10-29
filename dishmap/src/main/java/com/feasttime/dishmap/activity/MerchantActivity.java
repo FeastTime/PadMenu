a610f8285c6a82a3afe6575cbb3b8860225abf7d
Chen <38033919@qq.com>
2017-10-29 10:34:16 +0800

    增加商家，到店确认页面

--- dishmap/src/main/java/com/feasttime/dishmap/activity/MerchantActivity.java
+++ dishmap/src/main/java/com/feasttime/dishmap/activity/MerchantActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -15,12 +16,13 @@ import com.feasttime.dishmap.utils.UtilTools;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by chen on 2017/10/28.
  */
 
-public class MerchantActivity extends BaseActivity {
+public class MerchantActivity extends BaseActivity implements View.OnClickListener{
 
     @Bind(R.id.activity_merchant_content_fl)
     FrameLayout containerFl;
@@ -35,11 +37,9 @@ public class MerchantActivity extends BaseActivity {
     TextView mineTv;
 
 
-    private FragmentManager mFragmentManager;
     private MerchantOpenTableFragment merchantOpenTableFragment;
     private MerchantMineFragment merchantMineFragment;
     private MerchantReachStoreConfirmFragment merchantReachStoreConfirmFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,15 +58,70 @@ public class MerchantActivity extends BaseActivity {
         UtilTools.chenageTextDrawableSize(reachStoreConfirmTv,R.mipmap.confirm,picSize2,picSize2);
         UtilTools.chenageTextDrawableSize(mineTv,R.mipmap.mine,picSize3,picSize4);
 
+        opentTableTv.performClick();
 
-        merchantOpenTableFragment = new MerchantOpenTableFragment();
-        merchantMineFragment = new MerchantMineFragment();
-        merchantReachStoreConfirmFragment = new MerchantReachStoreConfirmFragment();
-
-        mFragmentManager = getFragmentManager();
-        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.add(R.id.activity_merchant_content_fl,merchantOpenTableFragment).commit();
+//        merchantOpenTableFragment = new MerchantOpenTableFragment();
+//
+//        mFragmentManager = getFragmentManager();
+//        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.activity_merchant_content_fl,merchantOpenTableFragment).commit();
     }
 
 
+    @OnClick({R.id.activity_merchant_open_table_tv,R.id.activity_merchant_reach_store_confirm_tv,R.id.activity_merchant_mine_tv})
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        hideAllFragment(fragmentTransaction);
+        if (v == opentTableTv) {
+            if (merchantOpenTableFragment == null) {
+                merchantOpenTableFragment = new MerchantOpenTableFragment();
+            }
+
+            if (merchantOpenTableFragment.isAdded()) {
+                fragmentTransaction.show(merchantOpenTableFragment).commitAllowingStateLoss();
+            } else {
+                fragmentTransaction.add(R.id.activity_merchant_content_fl, merchantOpenTableFragment);
+                fragmentTransaction.show(merchantOpenTableFragment).commitAllowingStateLoss();
+            }
+        } else if (v == reachStoreConfirmTv) {
+            if (merchantReachStoreConfirmFragment == null) {
+                merchantReachStoreConfirmFragment = new MerchantReachStoreConfirmFragment();
+            }
+
+            if (merchantReachStoreConfirmFragment.isAdded()) {
+                fragmentTransaction.show(merchantReachStoreConfirmFragment).commitAllowingStateLoss();
+            } else {
+                fragmentTransaction.add(R.id.activity_merchant_content_fl, merchantReachStoreConfirmFragment);
+                fragmentTransaction.show(merchantReachStoreConfirmFragment).commitAllowingStateLoss();
+            }
+        } else if (v == mineTv) {
+            if (merchantMineFragment == null) {
+                merchantMineFragment = new MerchantMineFragment();
+            }
+
+            if (merchantMineFragment.isAdded()) {
+                fragmentTransaction.show(merchantMineFragment).commitAllowingStateLoss();
+            } else {
+                fragmentTransaction.add(R.id.activity_merchant_content_fl, merchantMineFragment);
+                fragmentTransaction.show(merchantMineFragment).commitAllowingStateLoss();
+            }
+        }
+
+
+    }
+
+    private void hideAllFragment(FragmentTransaction fragmentTransaction) {
+        if (merchantMineFragment != null && merchantMineFragment.isAdded()) {
+            fragmentTransaction.hide(merchantMineFragment);
+        }
+
+        if (merchantOpenTableFragment != null && merchantOpenTableFragment.isAdded()) {
+            fragmentTransaction.hide(merchantOpenTableFragment);
+        }
+
+        if (merchantReachStoreConfirmFragment != null && merchantReachStoreConfirmFragment.isAdded()) {
+            fragmentTransaction.hide(merchantReachStoreConfirmFragment);
+        }
+    }
 }
---
+++ dishmap/src/main/java/com/feasttime/dishmap/model/bean/ReachStoreConfirmItemInfo.java
@@ -0,0 +1,35 @@
+package com.feasttime.dishmap.model.bean;
+
+/**
+ * Created by chen on 2017/10/29.
+ */
+
+public class ReachStoreConfirmItemInfo {
+    String name;
+    String number;
+    String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
