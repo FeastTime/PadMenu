904b0328b5eb28a3c72755c5da8165295d67740d
Chen <38033919@qq.com>
2018-01-10 11:44:06 +0800

    增加fragment

---
+++ dishmap/src/main/java/com/feasttime/dishmap/fragment/UserMineFragment.java
@@ -0,0 +1,30 @@
+package com.feasttime.dishmap.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chen on 2018/1/10.
+ */
+
+public class UserMineFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
