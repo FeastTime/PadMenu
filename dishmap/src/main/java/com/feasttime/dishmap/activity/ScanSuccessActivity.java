d70f0d34262bfa76da3f065b48a9e75a3a90b191
Chen <38033919@qq.com>
2018-01-11 17:52:29 +0800

    增加扫码成功页面

---
+++ dishmap/src/main/java/com/feasttime/dishmap/activity/ScanSuccessActivity.java
@@ -0,0 +1,25 @@
+package com.feasttime.dishmap.activity;
+
+import android.os.Bundle;
+
+import com.feasttime.dishmap.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by chen on 2018/1/11.
+ */
+
+public class ScanSuccessActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_success);
+        ButterKnife.bind(this);
+
+
+    }
+}
