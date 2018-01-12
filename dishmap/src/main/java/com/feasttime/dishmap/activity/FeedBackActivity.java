fb8933dbf61f2f9c5850dda835f5cbd9ad00992b
Chen <38033919@qq.com>
2018-01-12 17:51:05 +0800

    增加意见反馈和我的座位页面

---
+++ dishmap/src/main/java/com/feasttime/dishmap/activity/FeedBackActivity.java
@@ -0,0 +1,14 @@
+package com.feasttime.dishmap.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by chen on 2018/1/12.
+ */
+
+public class FeedBackActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ dishmap/src/main/java/com/feasttime/dishmap/activity/MySeatActivity.java
@@ -0,0 +1,14 @@
+package com.feasttime.dishmap.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by chen on 2018/1/12.
+ */
+
+public class MySeatActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
