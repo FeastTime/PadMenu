772e5ec27b13a330229d9a92599a24f8ec9edf1c
Chen <38033919@qq.com>
2018-01-12 18:18:26 +0800

    修改反馈页面

--- dishmap/src/main/java/com/feasttime/dishmap/activity/FeedBackActivity.java
+++ dishmap/src/main/java/com/feasttime/dishmap/activity/FeedBackActivity.java
@@ -1,14 +1,46 @@
 package com.feasttime.dishmap.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.feasttime.dishmap.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by chen on 2018/1/12.
  */
 
 public class FeedBackActivity extends BaseActivity {
+
+    @Bind(R.id.title_back_iv)
+    ImageView titleBarBackIv;
+
+    @Bind(R.id.title_bar_right_iv)
+    ImageView titleBarRightIv;
+
+    @Bind(R.id.title_center_text_tv)
+    TextView titleCenterTv;
+
+    @Bind(R.id.title_bar_layout_orange_bg_iv)
+    ImageView titleBarOrangeBgIv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        ButterKnife.bind(this);
+        initViews();
+    }
+
+    private void initViews() {
+        titleBarOrangeBgIv.setVisibility(View.GONE);
+        titleCenterTv.setText("意见反馈");
+        titleCenterTv.setTextColor(this.getResources().getColor(R.color.text_gray_1));
+        titleBarRightIv.setVisibility(View.GONE);
+        titleBarBackIv.setImageResource(R.mipmap.gray_back_icon);
     }
 }
