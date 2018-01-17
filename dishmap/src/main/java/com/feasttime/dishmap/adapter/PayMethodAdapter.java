c71157f0a8ffa5fb35e247aa3921ebb2804ce741
Chen <38033919@qq.com>
2018-01-17 09:32:57 +0800

    修改支付方式

--- dishmap/src/main/java/com/feasttime/dishmap/adapter/PayMethodAdapter.java
+++ dishmap/src/main/java/com/feasttime/dishmap/adapter/PayMethodAdapter.java
@@ -6,15 +6,12 @@ package com.feasttime.dishmap.adapter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.media.ThumbnailUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.feasttime.dishmap.R;
 import com.feasttime.dishmap.model.bean.PayMethodItemInfo;
 
