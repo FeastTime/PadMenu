8d1b4ae47d40d6c8cf799e299eb7a609a3dc267a
Chen <38033919@qq.com>
2018-01-18 19:42:30 +0800

    增加根据坐标算距离的方法

--- dishmap/src/main/java/com/feasttime/dishmap/utils/UtilTools.java
+++ dishmap/src/main/java/com/feasttime/dishmap/utils/UtilTools.java
@@ -23,6 +23,8 @@ import java.util.HashMap;
  */
 
 public class UtilTools {
+    private static final double EARTH_RADIUS = 6378137.0;
+
     public static int[] getImageWidthHeight(Context context, int resid){
         BitmapFactory.Options options = new BitmapFactory.Options();
 
@@ -106,5 +108,23 @@ public class UtilTools {
             result = context.getResources().getDimensionPixelSize(resourceId);
         }
         return result;
+    }
+
+    // 返回单位是米
+    public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
+        double Lat1 = rad(latitude1);
+        double Lat2 = rad(latitude2);
+        double a = Lat1 - Lat2;
+        double b = rad(longitude1) - rad(longitude2);
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(Lat1) * Math.cos(Lat2)
+                * Math.pow(Math.sin(b / 2), 2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s;
+    }
+
+    private static double rad(double d) {
+        return d * Math.PI / 180.0;
     }
 }
