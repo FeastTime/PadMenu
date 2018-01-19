03148dcc2db59f7f5b35deeb99cac600584b3e31
Chen <38033919@qq.com>
2018-01-19 15:46:15 +0800

    增加关于页面

--- dishmap/src/main/java/com/feasttime/dishmap/activity/AboutActivity.java
+++ dishmap/src/main/java/com/feasttime/dishmap/activity/AboutActivity.java
@@ -1,9 +1,17 @@
 package com.feasttime.dishmap.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.feasttime.dishmap.R;
+import com.feasttime.dishmap.adapter.MessageAdapter;
+import com.feasttime.dishmap.model.bean.MessageItemInfo;
 
+import java.util.ArrayList;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -12,10 +20,49 @@ import butterknife.ButterKnife;
 
 public class AboutActivity extends BaseActivity {
 
+    @Bind(R.id.title_back_iv)
+    ImageView titleBarBackIv;
+
+    @Bind(R.id.title_bar_right_iv)
+    ImageView titleBarRightIv;
+
+    @Bind(R.id.title_center_text_tv)
+    TextView titleCenterTv;
+
+    @Bind(R.id.title_bar_layout_orange_bg_iv)
+    ImageView titleBarOrangeBgIv;
+
+    @Bind(R.id.activity_about_version_tv)
+    TextView versionTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
+        initViews();
+    }
+
+
+    private void initViews() {
+        titleBarOrangeBgIv.setVisibility(View.GONE);
+        titleCenterTv.setText("关于");
+        titleCenterTv.setTextColor(this.getResources().getColor(R.color.text_gray_1));
+        titleBarRightIv.setVisibility(View.GONE);
+        titleBarBackIv.setImageResource(R.mipmap.gray_back_icon);
+
+        versionTv.setText("V" + getVersionName());
+    }
+
+
+    private String getVersionName() {
+        try {
+            String pkName = this.getPackageName();
+            String versionName = this.getPackageManager().getPackageInfo(
+                    pkName, 0).versionName;
+            return versionName;
+        } catch (Exception e) {
+        }
+        return null;
     }
 }
