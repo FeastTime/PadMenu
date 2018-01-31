18925368326f75226bff3479a7577f5c867e9355
Chen <38033919@qq.com>
2018-01-31 16:45:56 +0800

    微信头像存sp

--- dishmap/src/main/java/com/feasttime/dishmap/wxapi/WXEntryActivity.java
+++ dishmap/src/main/java/com/feasttime/dishmap/wxapi/WXEntryActivity.java
@@ -127,6 +127,7 @@ public class WXEntryActivity extends BaseActivity implements IWXAPIEventHandler 
 
                             //保存微信用户openid
                             PreferenceUtil.setStringKey(PreferenceUtil.WE_CHAT_OPENID,openid);
+                            PreferenceUtil.setStringKey(PreferenceUtil.USER_ICON,headimgurl);
 
                             //上传微信用户数据
 
