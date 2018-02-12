c489f4ad4e1e46c304ef0adf105636fa9a156d6a
Xiaoqing.Li <xiaoqing.li@ipinyou.com>
2018-02-12 09:07:33 +0800

    座位展示修改

--- dishmap/src/main/java/com/feasttime/dishmap/adapter/MySeatAdapter.java
+++ dishmap/src/main/java/com/feasttime/dishmap/adapter/MySeatAdapter.java
@@ -1,37 +1,35 @@
 package com.feasttime.dishmap.adapter;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.feasttime.dishmap.R;
-import com.feasttime.dishmap.model.bean.MessageItemInfo;
 import com.feasttime.dishmap.model.bean.MyTableItemInfo;
-
 import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
-import cn.nekocode.badge.BadgeDrawable;
 
 /**
+ *
  * Created by chen on 2018/1/26.
  */
 
 public class MySeatAdapter extends BaseAdapter {
     private List<MyTableItemInfo> dataList;
-    private Context context;
+//    private Context context;
     private LayoutInflater mLayoutInflater;
     private SimpleDateFormat simpleDateFormat;
 
     public MySeatAdapter(Context context,List<MyTableItemInfo> dataList) {
+
         mLayoutInflater = LayoutInflater.from(context);
         this.dataList = dataList;
-        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
     }
 
     @Override
@@ -51,7 +49,7 @@ public class MySeatAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
+        ViewHolder holder;
         if (convertView == null) {
             convertView = mLayoutInflater.inflate(R.layout.activity_my_seat_lv_item, null);
             holder = new ViewHolder();
@@ -76,7 +74,9 @@ public class MySeatAdapter extends BaseAdapter {
             holder.statusTv.setText("已验证");
         }
 
-        holder.timeTv.setText(simpleDateFormat.format(myTableItemInfo.getMaketableTime()));
+        long expireTime = myTableItemInfo.getTaketableTime() + myTableItemInfo.getRecieveTime()*60*1000;
+
+        holder.timeTv.setText(simpleDateFormat.format(expireTime));
 
         return convertView;
     }
